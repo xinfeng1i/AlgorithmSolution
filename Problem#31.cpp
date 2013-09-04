a4ba96311a0072b748df02ea0703568dc61af9c3
python27 <silfer.lee@gmail.com>
2013-09-04 12:00:48 +0800

    dynamic programming, last time init situation wrong, revised it, good

--- Problem#31.cpp
+++ Problem#31.cpp
@@ -4,24 +4,25 @@
 using namespace std;
 
 int coins[8] = {1, 2, 5, 10, 20, 50, 100, 200};
+/*
+ * Dynamic Programming
+ *
+ * dp[i][j] = using the first i kinds of coins to construct sum j
+ * then dp[i][j] = dp[i-1][j - k * coins[i - 1]], k = 0, 1, ... ,j / coins[i-1]
+ *
+ * init
+ * dp[i][0] = 1, where i = 0, 1, 2, ... ,8
+ */
 int main()
 {
     int dp[9][201] = {0};
     //init
     for (int i = 0; i < 9; ++i)
     {
-        for (int j = 0; j < 201; ++j)
-        {
-            dp[i][j] = 0;
-        }
-    }
-    // base case
-    for (int j = 1; j < 201; ++j)
-    {
-        dp[1][j] = 1;
+        dp[i][0] = 1;
     }
 
-    for (int i = 2; i < 9; ++i)
+    for (int i = 1; i < 9; ++i)
     {
         for (int j = 1; j < 201; ++j)
         {
@@ -38,18 +39,6 @@ int main()
     cout << "the number of ways using 8 coins to make is : "
         << dp[8][200] << endl;
 
-    cout << "1st row:" << endl;
-    cout << "2rd row:" << endl;
-    for (int i = 0; i < 201; ++i)
-    {
-        cout << dp[1][i] << "," ;
-    }
-    cout << endl;
-    for (int i = 0; i < 201; ++i)
-    {
-        cout << dp[2][i] << "," ;
-    }
-    cout << endl;
     return 0;
 }
 
