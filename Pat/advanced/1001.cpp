a7c60b4e31021ef626eb1b805ae84b98ae6581f6
python27 <silfer.lee@gmail.com>
2013-09-16 11:47:52 +0800

    easy, good

---
+++ Pat/advanced/1001.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+/*
+int main()
+{
+    long long a = 0;
+    long long b = 0;
+    cin >> a >> b;
+    long long c = a + b;
+    if (abs(c) < 1000)
+    {
+        cout << c << endl;
+    }
+    else if (abs(c) < 1000000)
+    {
+        string str_c = to_string(c);
+        long sz = str_c.size();
+        cout << str_c.substr(0, sz-3) << "," << str_c.substr(sz-3, 3) << endl;
+    }
+    else
+    {
+        string str_c = to_string(c);
+        long sz = str_c.size();
+        cout << str_c.substr(0, sz-6) << "," << str_c.substr(sz-6, 3) 
+             << "," << str_c.substr(sz-3, 3) << endl;
+    }
+    return 0;
+}
+*/
