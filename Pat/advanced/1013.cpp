49066da009810e3f636554cef7268429c52c8c62
python27 <silfer.lee@gmail.com>
2013-09-21 23:15:28 +0800

    graph DFS algorithm, first implement it, good, but cost too much memory, TODO

--- Pat/advanced/1013.cpp
+++ Pat/advanced/1013.cpp
@@ -1,7 +1,67 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <set>
 using namespace std;
 
+class Graph
+{
+private:
+    int nodeNum_;
+    long edgeNum_;
+    vector<set<int> > edges_;
+    vector<bool> visited_;
+public:
+    Graph(int N, long M):nodeNum_(N), edgeNum_(M)
+    {
+        edges_.resize(N + 1);
+        visited_.resize(N + 1);
+    }
+    void addEdge(int s, int e)
+    {
+        edges_[s].insert(e);
+        edges_[e].insert(s); 
+    }
+
+    void setAllFalse()
+    {
+        for (int i = 0; i <= nodeNum_; ++i)
+        {
+            visited_[i] = false;
+        }
+    }
+    void setTrue(int v)
+    {
+        visited_[v] = true;
+    }
+
+    void explore(int v)
+    {
+        visited_[v] = true;
+        for (auto it = edges_[v].begin(); it != edges_[v].end(); ++it)
+        {
+            if (!visited_[*it])
+            {
+                explore(*it);
+            }
+        }
+    }
+
+    int cntConnectedParts(int excludedV)
+    {
+        int cnt = 0;
+        for (int i = 1; i <= nodeNum_; ++i)
+        {
+            if (i != excludedV && !visited_[i])
+            {
+                explore(i);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+};
 
 int main()
 {
@@ -9,15 +69,23 @@ int main()
     long M = 0;
     int K = 0;
     cin >> N >> M >> K;
-    vector<pair<int, int> > edges(M, pair<int, int>(0, 0));
+
+    Graph g(N, M);
+    int start, end;
     for (long i = 0; i < M; ++i)
     {
-        cin >> edges[i].first >> edges[i].second;
+        cin >> start >> end;
+        g.addEdge(start, end);
     }
-
-    vector<int> checkNodes(K, 0);
+    
+    int v = 0;
     for (int i = 0; i < K; ++i)
     {
-        cin >> checkNodes[i];
+        cin >> v;
+        g.setAllFalse();
+        g.setTrue(v);
+        int d = g.cntConnectedParts(v);
+        cout << d - 1 << endl;
     }
+    return 0;
 }
