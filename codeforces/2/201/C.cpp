94ed560019d51dcb68d35ab5a194b32a9b3335e1
python27 <silfer.lee@gmail.com>
2013-09-25 12:28:19 +0800

    use GCD of array to solve it elegant, refer to other's algorithm, good

--- codeforces/2/201/C.cpp
+++ codeforces/2/201/C.cpp
@@ -1,67 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <set>
 #include <string>
+#include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
+
+// Greatest Common Divisor between a and b
+long long gcd(long long a, long long b)
+{
+	while (b != 0)
+	{
+		long long r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+
+// return the greatest common divisor among all values in v
+// Time complexity:
+long long arrayGcd(vector<long long> v)
+{
+	long long result = v[0];
+	for (int i = 1; i < v.size(); ++i)
+	{
+		result = gcd(result, v[i]);
+	}
+
+    return result;
+}
 
 int main()
 {
-    int n = 0;
-    cin >> n;
-    vector<long long> v(n, 0);
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> v[i];
-    }
+	// read input
+	int n = 0;
+	cin >> n;
+	vector<long long> v(n, 0);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> v[i];
+	}
 
 
-    // Alice opration
-    bool Alicefound = false;
-    long long diff = 0;
-    while (true)
-    {
-AliceOperation:for (int i = 0; i < v.size(); ++i)
-        {
-            for (int j = i + 1; j < v.size(); ++j)
-            {
-                diff = abs(v[i] - v[j]);
-                if (count(v.begin(), v.end(), diff) == 0)
-                {
-                    v.push_back(diff);
-                    Alicefound = true;
-                    goto BobOperation;
-                }
-            }
-        }
+	long long max_elem = *max_element(v.begin(), v.end());
+	long long d = arrayGcd(v);
 
-        if (!Alicefound)
-        {
-            cout << "Bob" << endl;
-            break;
-        }
-
-        // Bob operation
-BobOperation: for (int i = 0; i < v.size(); ++i)
-        {
-            for (int j = i + 1; j < v.size(); ++j)
-            {
-                diff = abs(v[i] - v[j]);
-                if (count(v.begin(), v.end(), diff) == 0)
-                {
-                    v.push_back(diff);
-                    Alicefound = false;
-                    goto AliceOperation;
-                }
-            }
-        }
-
-        if (Alicefound)
-        {
-            cout << "Alice" << endl;
-            break;
-        }
-    }
-    return 0;
+	long long remain_round = max_elem / d - v.size();
+	if (remain_round % 2 == 1)
+	{
+		cout << "Alice" << endl;
+	}
+	else
+	{
+		cout << "Bob" << endl;
+	}
+	return 0;
 }
+
+
+// Summary:
+// 1. calculate GCD use Euler Algorithm
+// 2. calculate other GCD always depends on Euler's GCD Algorithm
+// 3. figure out why we can achieve all the differece |x-y| in such
+//    way, what's behind ? Number Theory ?
