ff11bc8ccd5f9e58473f45c1418d7cac3643f7b5
python27 <silfer.lee@gmail.com>
2013-09-26 22:28:44 +0800

    complex sorting and file IO, do really good

--- Pat/advanced/1025.cpp
+++ Pat/advanced/1025.cpp
@@ -1,112 +1,104 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
 using namespace std;
 
-class Student
+struct Student
 {
-private:
     string id_;
-    int grade_;
+    int score_;
     int location_;
-    int rank_;
-public:
-    // constructor
-    Student(string name, int marks, int location=0):id_(name),
-                                                     grade_(marks),
-                                                     location_(location),
-                                                     rank_(0)
-    {}
-    
-    // destructor
-    ~Student()
-    {}
-
-    // overload operator< in order to sort the students
-    bool operator<(const Student& other) const;
-
-    friend ostream& operator<<(ostream& os, const Student& s);
-
-    friend istream& operator>>(istream& is, const Student& s);
-
-    inline int getGrade() const
-    {
-        return grade_;
-    }
-    inline void setRank(int r)
-    {
-        rank_ = r;
-    }
-    
-    inline void setLocation(int l)
-    {
-        location_ = l;
-    }
+    int localRank_;
+    int finalRank_;
 };
 
-// overload operator<
-bool Student::operator<(const Student& other) const
+bool compareStudent(const Student &a, const Student& b)
 {
-    if (grade_ > other.grade_)
+    if (a.score_ == b.score_)
     {
-        return true;
-    }
-    else if (grade_ < other.grade_)
-    {
-        return false;
+        return a.id_ < b.id_;
     }
     else
     {
-        if (id_ < other.id_)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        return a.score_ > b.score_;
     }
 }
 
-// overload operator<< 
-ostream& operator<<(ostream& os, const Student& s)
-{
-    os << s.id_ << " " << s.grade_ << " " << s.location_;
-    return os;
-}
-
-istream& operator>>(istream& is, const Student& s)
-{
-    is >> s.id_ >> s.grade_;
-    return is;
-}
-
-
-typedef vector<Student> studentList;
 int main()
 {
+    //freopen("1025data.txt", "r", stdin);
     int N = 0;
     int K = 0;
     cin >> N;
-    vector<studentList> allStudents(N);
+    vector<Student> list;
     for (int i = 0; i < N; ++i)
     {
         cin >> K;
-        allStudents[i].resize(K);
+        vector<Student> tmplist(K);     //tmplist to sort local lists
+
+        // read local list, set the local number
         for (int j = 0; j < K; ++j)
         {
-            cin >> allStudents[i][j];
-            allStudents[i][j].setLocation(i + 1);
+            cin >> tmplist[j].id_ >> tmplist[j].score_;
+            tmplist[j].location_ = i + 1;
         }
+        // sort local list
+        sort(tmplist.begin(), tmplist.end(), compareStudent);
 
-    }
-
-    for (int i = 0; i < N; ++i)
-    {
-        sort(allStudents[i].begin(), allStudents[i].end());
-        for (int j = 0; j < allStudents[i].size(); ++j)
+        vector<int> tmpscores;  // helper vector only contained local scores
+        for (int j = 0; j < K; ++j)
         {
-            allStudents[i][j].getGrade();
+            tmpscores.push_back(tmplist[j].score_);
         }
+
+        
+        // calculate local rank
+        for (int j = 0; j < K; ++j)
+        { 
+            auto low = lower_bound(tmpscores.begin(), 
+                                   tmpscores.end(),
+                                   tmplist[j].score_,
+                                   greater<int>());
+            tmplist[j].localRank_ = low - tmpscores.begin() + 1;
+        }
+
+        // append local list to the global list
+        list.insert(list.end(), tmplist.begin(), tmplist.end());
     }
+    
+    // sort global list
+    sort(list.begin(), list.end(), compareStudent);
+
+    
+    // extract all the scores int to a vector
+    vector<int> globalScores(list.size(), 0);
+    for (int i = 0; i < list.size(); ++i)
+    {
+        globalScores[i] = list[i].score_;
+    }
+
+    // calculate global rank
+    for (int i = 0; i < list.size(); ++i)
+    {
+        auto low = lower_bound(globalScores.begin(),
+                               globalScores.end(),
+                               list[i].score_, 
+                               greater<int>());
+        list[i].finalRank_ = low - globalScores.begin() + 1;
+    }
+
+    cout << list.size() << endl;
+    for (int i = 0; i < list.size(); ++i)
+    {
+        cout << list[i].id_ << " "
+             << list[i].finalRank_ << " "
+             << list[i].location_ << " "
+             << list[i].localRank_ << endl;
+    }
+
+    return 0;
+
 }
