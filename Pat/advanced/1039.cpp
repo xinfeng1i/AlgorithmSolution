d5b050b732044b1d0de3953d909c502b0b68ce57
python27 <silfer.lee@gmail.com>
2013-09-29 22:48:10 +0800

    elegant data structrue, but last sample overtime, I believe it's because c++ I/O, TODO

---
+++ Pat/advanced/1039.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    //freopen("1039data.txt", "r", stdin);
+
+    long queryNumber = 0;
+    int courseNumber = 0;
+    cin >> queryNumber >> courseNumber;
+
+    map<string, set<int> > table;
+    int courseIndex = 0;
+    long studentNumber = 0;
+    string name;
+    for (int i = 0; i < courseNumber; ++i)
+    {
+        cin >> courseIndex >> studentNumber;
+        for (int k = 0; k < studentNumber; ++k)
+        {
+            cin >> name;
+            table[name].insert(courseIndex);
+        }
+    }
+
+    vector<string> querylist(queryNumber);
+    string queryName;
+    for (long i = 0; i < queryNumber; ++i)
+    {
+        cin >> queryName;
+        int registerNumber = table[queryName].size();
+        cout << queryName << " " << registerNumber;
+        if (registerNumber == 0)
+        {
+            //cout << endl;
+            printf("\n");
+        }
+        else
+        {
+            for (auto it = table[queryName].begin(); 
+                      it != table[queryName].end(); ++it)
+            {
+                //cout << " " << *it;
+                printf(" %d", *it);
+            }
+            //cout << endl;
+            printf("\n");
+        }
+    }
+    
+/*
+    string queryName;
+    for (long i = 0; i < queryNumber; ++i)
+    {
+        queryName = querylist[i];
+        int registerNumber = table[queryName].size();
+        cout << queryName << " " << registerNumber;
+        if (registerNumber == 0)
+        {
+            //cout << endl;
+            printf("\n");
+        }
+        else
+        {
+            for (auto it = table[queryName].begin(); 
+                      it != table[queryName].end(); ++it)
+            {
+                //cout << " " << *it;
+                printf(" %d", *it);
+            }
+            //cout << endl;
+            printf("\n");
+        }
+    }
+*/
+    return 0;
+}
