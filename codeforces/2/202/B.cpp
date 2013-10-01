7facf2176dffecef2b7c8ffac874ab128c314c3b
python27 <silfer.lee@gmail.com>
2013-10-01 22:26:27 +0800

    refer to others' solution, reimplement it

--- codeforces/2/202/B.cpp
+++ codeforces/2/202/B.cpp
@@ -5,63 +5,50 @@
 #include <algorithm>
 using namespace std;
 
-bool isAllZero(long a[], int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        if (a[i] != 0)
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-long minElem(long a[], int n)
-{
-    long min = 1000000;
-    for (int i = 0; i < n; ++i)
-    {
-        if (a[i] != 0 && a[i] < min)
-        {
-            min = a[i];
-        }
-    }
-
-    return min;
-}
-
 int main()
 {
-    long v;
-    cin >> v;
+    long totalv;
+    cin >> totalv;
     long a[10] = {0};
     for (int i = 1; i < 10; ++i)
     {
         cin >> a[i];
     }
 
-    for (int i = 0; i < 10; ++i)
-    {
-        if (a[i] > v)
-        {
-            a[i] = 0;
-        }
-    }
+    long minv = *min_element(a + 1, a + 10); 
     
-    if (isAllZero(a, 10))
+    // special case: min_v > totalv
+    // return -1
+    if (totalv < minv)
     {
         cout << -1 << endl;
         return 0;
     }
 
-    long minimum = minElem(a, 10);
-    for (int i = 9; i >= 1; --i)
+    long digit_num = totalv / minv;
+    long remainder = totalv % minv; 
+    for (int i = 0; i < digit_num; ++i)
     {
-        if (a[i] == minimum)
+        int k = 9;
+        while (k >= 0 && a[k] >  minv + remainder)
         {
-            
+            k--;
         }
+        remainder = minv + remainder - a[k];
+        cout << k;
+
     }
+    cout << endl;
+    return 0;
 }
+
+// I almost catch the whole idea, so close.
+//
+// Main Idea:
+// choose digit which cost least paint, to contain more digits, thus make
+// the number more big, calculate the number of digits, total paint divide
+// the chosen digit, which may still have 0, 1, 2, ..., digit-1 paint
+// for 9 to 1 to check if we can paint the larger number.
+//
+// the tag of the problem is DP, Greedy, Binay Search
+// But I cannot find any clue about that
