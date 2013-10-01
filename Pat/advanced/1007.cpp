6a756fb66d61086d75663088e714144b64e352e0
python27 <silfer.lee@gmail.com>
2013-10-01 16:47:48 +0800

    classical dp problem, max sum subsequence, lookup wiki, remember

--- Pat/advanced/1007.cpp
+++ Pat/advanced/1007.cpp
@@ -1,57 +1,91 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
 #include <vector>
+#include <algorithm>
+#include <cstdio>
 using namespace std;
 
-long maxSubsequenceSum(vector<long long> &v, long &i, long &j)
+long g_begin = 0;
+long g_end = 0;
+////////////////////////////////////////////////////////////
+// Classical Dynamic Programming Problem
+// Max sum of subSequence
+//
+// Define subproblem as:
+// sum[j] = the max subsequce sum ending up with j
+// for the next element indexed as j + 1
+// if sum[j] < 0, the elem[j+1] will indenpent, so
+// sum[j + 1] = elem[j+1]
+// else, the elem[j+1] must append the sum[j]
+// sum[j+1] += elem[j+1]
+//
+// Recursive Relation:
+// ********************************
+// * sum[j] = v[j]   sum[j-1] < 0 *
+// * sum[j] += v[j]  other wise   *
+// ********************************
+// Constrain: array must have at least one nonpositive element
+// Special Case: all elems are negetive
+// 
+// Input:
+// v - array
+// Output:
+// max sum of subsequence of array
+//////////////////////////////////////////////////////////////
+long maxSubsequenceSum(const vector<long>& v)
 {
-    // special case, all < 0
     if (*max_element(v.begin(), v.end()) < 0)
     {
-        i = 0;
-        j = v.size() - 1;
+        g_begin = 0;
+        g_end = static_cast<long>(v.size()) - 1;        
         return 0;
     }
-    
-    long long maxSum = 0;
-    long maxi = 0;
-    long maxj = 0;
-    for (i = 0; i < v.size(); ++i)
-    {
-        for (j = 0; j < v.size(); ++j)
-        {
-            long long curSum = accumulate(v.begin() + i, v.begin() + j + 1, 0);
-            if (curSum > maxSum)
-            {
-                maxSum = 0;
-                maxi = i;
-                maxj = j;
-            }
-        }
-            
-    }
-    i = maxi;
-    j = maxj;
+    long max_sum_ending = v[0];
+    long tmp_start = 0;
+    long tmp_end = 0;
 
-    return maxSum;
+    long max_sum_sofar = v[0];
+    long sofar_start = 0;
+    long sofar_end = 0;
+
+    for (long i = 1; i < v.size(); ++i)
+    {
+        if (max_sum_ending < 0)
+        {
+            max_sum_ending = v[i];
+            tmp_start = i;
+            tmp_end = i;
+        }
+        else
+        {
+            max_sum_ending += v[i];
+            tmp_end = i;
+        }
+
+        if (max_sum_ending > max_sum_sofar)
+        {
+            max_sum_sofar = max_sum_ending;
+            sofar_start = tmp_start;
+            sofar_end = tmp_end;
+        }
+    }
+    g_begin = sofar_start;
+    g_end = sofar_end;
+
+    return max_sum_sofar;
 }
 
 int main()
 {
-    int N = 0;
-    cin >> N;
-    vector<long long> v(N, 0);
-    for (int k = 0; k < N; ++k)
+    int k = 0;
+    cin >> k;
+    vector<long> v(k, 0);
+    for (int i = 0; i < k; ++i)
     {
-        cin >> v[k];
+        cin >> v[i];
     }
 
-    long beginIndex = 0;
-    long endIndex = 0;
-    cout << maxSubsequenceSum(v, beginIndex, endIndex)<< " " ;
-    cout << v[beginIndex] << " " << v[endIndex] << endl;
+    cout << maxSubsequenceSum(v);
+    cout << " " << v[g_begin] << " " << v[g_end] << endl;
     return 0;
-
 }
