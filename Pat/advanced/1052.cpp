fec88fb69991efa9c698c8cf94e966e65fba7955
python27 <silfer.lee@gmail.com>
2013-10-08 10:21:05 +0800

    not all nodes are valid nodes, also consider empty list. good

--- Pat/advanced/1052.cpp
+++ Pat/advanced/1052.cpp
@@ -22,27 +22,57 @@ int main()
     string head;
     cin >> n >> head;
 
+
     vector<Node> v(n);
     for (long i = 0; i < n; ++i)
     {
         cin >> v[i].address_ >> v[i].key_ >> v[i].next_; 
     }
 
-    sort (v.begin(), v.end(), compareNode);
-
-    cout << n << " " << v[0].address_ << endl;
-    for (long i = 0; i < n; ++i)
+    // linklist is empty, return 
+    if (head == "-1")
     {
-        if (i != n -1)
+        cout << "0 -1" << endl;
+        return 0;
+    }
+
+    // v2 contains all the valid nodes
+    vector<Node> v2;
+    while (head != "-1")
+    {
+        for (long i = 0; i < n; ++i)
         {
-            v[i].next_ = v[i+1].address_;
+            if (v[i].address_ == head)
+            {
+                v2.push_back(v[i]);
+                head = v[i].next_;
+            }
+        }
+    }
+
+    // no valid nodes, return 
+    if (v2.size() == 0)
+    {
+        cout << "0 -1" << endl;
+        return 0;
+    }
+
+    sort (v2.begin(), v2.end(), compareNode);
+
+    long sz = v2.size();
+    cout << sz << " " << v2[0].address_ << endl;
+    for (long i = 0; i < sz; ++i)
+    {
+        if (i != sz -1)
+        {
+            v2[i].next_ = v2[i+1].address_;
         }
         else
         {
-            v[i].next_ = "-1";
+            v2[i].next_ = "-1";
         }
 
-        cout << v[i].address_ << " " << v[i].key_ << " " << v[i].next_ << endl;
+        cout << v2[i].address_ << " " << v2[i].key_ << " " << v2[i].next_ << endl;
     }
 
     return 0;
