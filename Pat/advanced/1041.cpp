0a9f781519311c6ba35e473f24365876dfd5a5cb
python27 <silfer.lee@gmail.com>
2013-10-08 16:21:28 +0800

    using hash function to accelerate the query speed, perfect

--- Pat/advanced/1041.cpp
+++ Pat/advanced/1041.cpp
@@ -3,27 +3,32 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <functional>
 #include <cstdio>
 using namespace std;
 
+#define smax 65536
+
 int main()
 {
+    hash<long> myhash;
+
     long n = 0;
     cin >> n;
 
-    map<int, int>cnt;
+    int cnt[smax] = {0};
     vector<int> numbers(n, 0);
     int tmp = 0;
     for (long i = 0; i < n; ++i)
     {
         scanf("%d", &tmp);
         numbers[i] = tmp;
-        cnt[tmp]++;
+        cnt[myhash(tmp)]++;
     }
 
     for (long i = 0; i < n; ++i)
     {
-        if (cnt[numbers[i]] == 1)
+        if (cnt[myhash(numbers[i])] == 1)
         {
             //cout << numbers[i] << endl;
             printf("%d\n", numbers[i]);
