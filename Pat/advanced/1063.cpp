a40a327379594cd0400e5cac27661f720aab2238
python27 <silfer.lee@gmail.com>
2013-10-10 14:07:38 +0800

    use C scanf/printf I/O pass the last sample. what a shame

--- Pat/advanced/1063.cpp
+++ Pat/advanced/1063.cpp
@@ -9,30 +9,35 @@ using namespace std;
 int main()
 {
     int n = 0;
-    cin >> n;
+    //cin >> n;
+    scanf("%d", &n);
     vector<set<long> > setList(n);
 
     long m = 0;
     long tmp = 0;
     for (int i = 0; i < n; ++i)
     {
-        cin >> m;
+        //cin >> m;
+        scanf("%ld", &m);
         for (int j = 0; j < m; ++j)
         {
-            cin >> tmp;
+            //cin >> tmp;
+            scanf("%ld", &tmp);
             setList[i].insert(tmp);
         }
     }
 
     int k = 0;
-    cin >> k;
+    //cin >> k;
+    scanf("%d", &k);
     int s, e;
 
     vector<long> interSet(10000);
     vector<long> unionSet(20000);
     for (int i = 0; i < k; ++i)
     {
-        cin >> s >> e;
+        //cin >> s >> e;
+        scanf("%d %d", &s, &e);
         auto it1 = set_intersection(setList[s-1].begin(), setList[s-1].end(),
                                     setList[e-1].begin(), setList[e-1].end(),
                                     interSet.begin());
