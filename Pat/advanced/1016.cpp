ecd243b09068f44e1db1f039a0ddff1d3f52ccb0
python27 <silfer.lee@gmail.com>
2013-10-12 19:06:47 +0800

    very complicated implement problem, solve it very good though not elegant, goood

--- Pat/advanced/1016.cpp
+++ Pat/advanced/1016.cpp
@@ -2,193 +2,235 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
-class dateTime
+int rate[24];
+
+class timeDate
 {
-private:
+public:
     int month_;
-    int date_;
+    int day_;
     int hour_;
     int minute_;
 public:
-    explicit dateTime(const string& s)
+    timeDate(string time)
     {
-        month_ = 10 * (s[0] - '0') + (s[1] - '0');
-        date_ = 10 * (s[3] - '0') + (s[4] - '0');
-        hour_ = 10 * (s[6] - '0') + (s[7] - '0');
-        minute_ 10 * (s[9] - '0') + (s[10] - '0');
+        month_ = (time[0] - '0') * 10 + (time[1] - '0');
+        day_ = (time[3] - '0') * 10 + (time[4] - '0');
+        hour_ = (time[6] - '0') * 10 + (time[7] - '0');
+        minute_ = (time[9] - '0') * 10 + (time[10] - '0');
     }
-
-    ~dateTime()
+    timeDate():month_(0), day_(0), hour_(0), minute_(0)
     {}
-
-    bool operator<(const dateTime& rhs) const
+    
+    void printTime()
     {
-        if (month_ < rhs.month_)
-        {
-            return true;
-        }
-        else if (month_ > rhs.month_)
-        {
-            return false;
-        }
-        else if (month_ == rhs.month_)
-        {
-            if (date_ < rhs.date_)
-            {
-                return true;
-            }
-            else if (date_ > rhs.date_)
-            {
-                return false;
-            }
-            else if (date_ == rhs.date_)
-            {
-                if (hour_ < rhs.hour_)
-                {
-                    return true;
-                }
-                else if (hour_ > rhs.hour_)
-                {
-                    return false;
-                }
-                else if (hour_ == rhs.hour_)
-                {
-                    if (minute_ < rhs.minute_)
-                    {
-                        return true;
-                    }
-                    else if (minute_ >= rhs.minute_)
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
+        printf("%02d:%02d:%02d:%02d\n", month_, day_, hour_, minute_);
     }
 
-    bool operator==(const dateTime& rhs) const
-    {
-        return (month_ == rhs.month_ && date_ == rhs.date_ &&
-                hour_ == rhs.hour_ && minute_ == rhs.minute_);
-    }
-
-    // return time_different
-    int substractTime(const dateTime& beg) const
-    {
-        if (date_ == beg.date_)
-        {
-            return (hour_ * 24 + minute_) - (beg.hour_ * 24 + beg.minute_);
-        }
-        else if(date_ > beg.date_)
-        {
-            return (((date_ - beg.date_) * 1440 + hour_ * 24 + minute_)
-                   - (beg.hour_ * 24 + beg.minute_));
-        }
-    }
+    friend bool operator<(const timeDate& a, const timeDate& b);
+    friend bool operator==(const timeDate& a, const timeDate& b);
+    friend bool operator!=(const timeDate& a, const timeDate& b);
 };
 
-struct phoneRecord
+bool operator<(const timeDate& a, const timeDate& b)
 {
-    string name_;
-    string time_;
-    string state_;
-
-    bool operator<(const phoneRecord& rhs) const
+    if (a.month_ != b.month_)
     {
-        if (name_ < rhs.name_)
+        return a.month_ < b.month_;
+    }
+    else
+    {
+        if (a.day_ != b.day_)
         {
-            return true;
-        }
-        else if (name_ > rhs.name_)
-        {
-            return false;
+            return a.day_ < b.day_;
         }
         else
         {
-            if (dateTime(time_) < dateTime(rhs.time_))
+            if (a.hour_ != b.hour_)
             {
-                return true;
+                return a.hour_ < b.hour_;
             }
             else
             {
-                return false;
+                return a.minute_ < b.minute_;
             }
         }
     }
+}
+
+bool operator==(const timeDate& a, const timeDate& b)
+{
+    return (a.month_ == b.month_ && a.day_ == b.day_ &&
+            a.hour_ == b.hour_ && a.minute_ == b.minute_);
+}
+
+bool operator!=(const timeDate& a, const timeDate& b)
+{
+    return !(a==b);
+}
+
+struct Record
+{
+    string name_;
+    timeDate time_;
+    string state_;
 };
 
-int caculateToll(dateTime beg_time, dateTime end_time, int total_time, int a[24])
+bool cmpRecord(const Record& a, const Record& b)
 {
-    if (beg_time.date_ == end_time.date_ && beg_time.hour_ == end_time.hour_)
+    if (a.name_ != b.name_)
     {
-        return a[beg_time.hour_] * (end_time.minute - begin_time.minute_);
+        return a.name_ < b.name_;
     }
-    else //if (beg_time.date_ == end_time.date_ && beg_time.hour_ != end_time.hour_)
+    else
     {
-        int sum = a[beg_time.hour_] * (60 - beg_time.minute_);
-        int cur_hour = beg_time.hour;
-        total_time -= (60 - beg_time.minute_);
-        cur_hour = (cur_hour + 1) % 24;
-        while (total_time > 60)
-        {
-            sum += a[cur_hour] * 60;
-            total_time -= 60;
-            cur_hour = (cur_hour + 1) % 24;
-        }
-        
-        sum += a[cur_hour] * total_time;
+        return a.time_ < b.time_;
+    }
+}
 
-        return sum;
+double computeFee(const Record& a, const Record& b)
+{
+    double cost = 0.0;
+    timeDate t1 = a.time_;
+    timeDate t2 = b.time_;
+    if (t1.day_ == t2.day_ && t1.hour_ == t2.hour_)
+    {
+        return (double)(t2.minute_ - t1.minute_) * rate[t1.hour_ % 24] / 100.0;
     }
+    else if (t1.day_ == t2.day_ && t1.hour_ != t2.hour_)
+    {
+        if (t1.minute_ != 0)
+        {
+            cost += rate[t1.hour_ % 24] / 100.00  * (double)(60 - t1.minute_);
+            t1.hour_ += 1;
+        }
+        if (t2.minute_ != 0)
+        {
+            cost += rate[t2.hour_ % 24] / 100.00  * (double)t2.minute_;
+        }
+        if (t1.hour_ != t2.hour_)
+        {
+            for (int k = t1.hour_; k < t2.hour_; ++k)
+            {
+                cost += rate[k % 24] / 100.00 * 60;
+            }
+        }
+
+        return cost;
+    }
+    else if (t1.day_ != t2.day_)
+    {
+        while (t2.day_ != t1.day_)
+        {
+            t2.day_--;
+            t2.hour_ += 24;
+        }
+        Record x, y;
+        x.name_ = "AAA";
+        y.name_ = "AAA";
+        x.time_ = t1;
+        y.time_ = t2;
+        x.state_ = "on-line";
+        y.state_ = "off-line";
+        return computeFee(x, y);
+    }
+}
+
+long computeDiffTime(const Record& a, const Record& b)
+{
+    timeDate t1 = a.time_;
+    timeDate t2 = b.time_;
+    long x1 = (24 * t1.day_ + t1.hour_) * 60 + t1.minute_;
+    long x2 = (24 * t2.day_ + t2.hour_) * 60 + t2.minute_;
+    return x2 - x1;
 }
 
 int main()
 {
-    int toll[24];
+    //freopen("1016data.txt", "r", stdin);
+
     for (int i = 0; i < 24; ++i)
     {
-        cin >> toll[i];
+        cin >> rate[i];
     }
 
-    int numRecord = 0;
-    cin >> numRecord;
-
-    vector<phoneRecord> list(numRecord);
-    for (int i = 0; i < numRecord; ++i)
+    int n;
+    cin >> n;
+    vector<Record> v(n);
+    string name, time, state;
+    for (int i = 0; i < n; ++i)
     {
-        cin >> list[i].name_ >> list[i].time_ >> list[i].state;
+        cin >> name >> time >> state;
+        v[i].name_ = name;
+        v[i].time_ = timeDate(time);
+        v[i].state_ = state;
     }
 
-    sort(list.begin(), list.end());
+    sort(v.begin(), v.end(), cmpRecord);
 
-    int sz = list.size();
-    int cur = 0;
-    while (cur < sz)
+    int ibegin = 0;
+    int iend = 0;
+    while (ibegin < n)
     {
-        int next = cur + 1;
-        while (list[next].name_ == list[cur].name_)
+        iend = ibegin;
+        while (iend < n && v[iend].name_ == v[ibegin].name_)
         {
-            next++;
+            iend++;
         }
-
-        cout << list[cur].name_ << " " << (list[cur].time_).substr(0, 2) << endl;
-
-        for (int i = cur; i < next; ++i)
+        
+        double totalcost = 0.0;
+        long totaldiffTime = 0;
+        
+        // judge whether this client has at least one valid pair
+        // if not, print nothing, continue to next loop
+        // else print the phone bills as sample
+        bool hasValidPair = false;
+        for (int k = ibegin; k < iend - 1; ++k)
         {
-            if (list[i].state_ == "on-line" && list[i+1].state_ == "off-line")
+            if (v[k].state_ == "on-line" && v[k+1].state_ == "off-line")
             {
-                dateTime beg_time(list[i].time_);
-                dateTime end_time(list[i+1].time_);
-                int total_time = end_time.substractTime(beg_time);
-                cout << (list[i].time_).substr(3,8) << " "
-                     << (list[i+1].time_).substr(3, 8) << " ";
-                // ToDo: print total time and total fee
+                hasValidPair = true;
             }
         }
+        if (hasValidPair)
+        {
+            cout << v[ibegin].name_  << " " << time.substr(0, 2) << endl;
+            for (int k = ibegin; k < iend - 1; )
+            {
+                if (v[k].state_ == "on-line" && v[k+1].state_ == "off-line")
+                {
+                    // compute and output
+                    double curcost = computeFee(v[k], v[k+1]);
+                    long curdiffTime = computeDiffTime(v[k], v[k+1]);
 
-        // TODO: print totoal fee
-        cout << "Total amount: " << "$" << ;
+                    totalcost += curcost;
+                    totaldiffTime += curdiffTime;
+
+                    printf("%02d:%02d:%02d ", 
+                            v[k].time_.day_, v[k].time_.hour_, v[k].time_.minute_);
+                    printf("%02d:%02d:%02d ",
+                            v[k+1].time_.day_, v[k+1].time_.hour_, 
+                            v[k+1].time_.minute_);
+
+                    printf("%ld ", curdiffTime);
+                    printf("$%0.2f\n", curcost);
+
+                    k += 2;
+                }
+                else
+                {
+                    k++; 
+                }
+            }
+
+            printf("Total amount: $%0.2f\n", totalcost);
+        }
+
+        ibegin = iend;
+
     }
+    return 0;
 }
