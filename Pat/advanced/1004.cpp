7eca1bca7f1b7a1d8db62758a0829227c24bd6fe
python27 <silfer.lee@gmail.com>
2013-10-17 23:47:31 +0800

    still confused

--- Pat/advanced/1004.cpp
+++ Pat/advanced/1004.cpp
@@ -7,7 +7,9 @@ using namespace std;
 struct Node
 {
     int id_;
-    vector<Node*> children;
+    vector<Node*> children_;
+    Node(int id):id_(id)
+    {}
 }
 
 int main()
@@ -15,19 +17,32 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    map<int, set<int> > childrenList;
-    int parent, nChildren, child;
+    map<int, vector<int> > table;
+    int id, k, child;
     for (int i = 0; i < m; ++i)
     {
-        cin >> parent >> nChildren; 
-        for (int j = 0; j < nChildren; ++j)
+        cin >> id >> k;
+        for (int j = 0; j < k; ++j)
         {
             cin >> child;
-            childrenList[parent].insert(child);
+            table[id].push_back(child);
         }
-
     }
 
-    vector<bool> created(n); 
-    
+    Node* root = new Node(1);
+    queue<Node*> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int sz = table[q.front()->id_].size();
+        for (int i =  0; i < sz; ++i)
+        {
+            Node* pnode = new Node(table[q.front()->id_][i]);
+            q.front()->children_.push_back(pnode);
+            q.push(pnode);
+        }
+        q.pop();
+    }
+
+
 }
