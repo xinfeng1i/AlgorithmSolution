0c603e413acd37385c09e1283e7c3f3f7e0e3790
python27 <silfer.lee@gmail.com>
2013-10-21 00:23:19 +0800

    self-implement bigInt class, do it right and good, but not elegant, to refer other's code

---
+++ Pat/advanced/1065.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct BigInt
+{
+    int a[25];  // the unsigned big int
+    bool flag;  // positive or negative
+
+    explicit BigInt(string s)
+    {
+        if (s[0] == '-')
+        {
+            flag = false;
+            s = s.substr(1);
+        }
+        else
+        {
+            flag = true;
+        }
+        for (int i = 0; i < 25; ++i)
+        {
+            a[i] = 0;
+        }
+        int sz = s.size();
+        for (int i = 0; i < sz; ++i)
+        {
+            a[i] = s[sz - 1 - i] - '0';
+        }
+    }
+
+    void printBigInt();
+    friend BigInt operator+(const BigInt& aa, const BigInt& bb);
+    bool greaterZero();
+    friend bool absGreater(const BigInt& aa, const BigInt& bb);
+};
+
+bool absGreater(const BigInt& aa, const BigInt& bb)
+{
+    string str1("");
+    string str2("");
+    for (int i = 24; i >=0; --i)
+    {
+        str1 += aa.a[i] + '0';
+    }
+    for (int i = 24; i >= 0; --i)
+    {
+        str2 += bb.a[i] + '0';
+    }
+    return str1 > str2;
+}
+
+
+bool BigInt::greaterZero()
+{
+    if (flag == false)
+    {
+        return false;
+    }
+    else
+    {
+        for (int i = 0; i < 25; ++i)
+        {
+            if (a[i] != 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+void BigInt::printBigInt()
+{
+    if (flag == false)
+    {
+        cout << "-";
+    }
+    int noZeroPos = 24;
+    while (noZeroPos >= 0 && a[noZeroPos] == 0)
+    {
+        noZeroPos--;
+    }
+    if (noZeroPos < 0)
+    {
+        cout << endl;
+        return;
+    }
+    else
+    {
+        for (int i = noZeroPos; i >= 0; i--)
+        {
+            cout << a[i];
+        }
+        cout <<endl;
+        return;
+    }
+}
+
+#if 1
+BigInt operator+(const BigInt& aa, const BigInt& bb)
+{
+    BigInt ans("0");
+    if (aa.flag == bb.flag)
+    {
+        ans.flag = aa.flag;
+        for (int i = 0; i < 25; ++i)
+        {
+            ans.a[i] = aa.a[i] + bb.a[i];
+        }
+        for (int i = 0; i < 24; ++i)
+        {
+            if (ans.a[i] >= 10)
+            {
+                ans.a[i+1] += ans.a[i] / 10;
+                ans.a[i] = ans.a[i] % 10;
+            }
+        }
+    }
+    else
+    {
+        if (absGreater(aa, bb))
+        {
+            ans.flag = aa.flag;
+            for (int i = 0; i < 25; ++i)
+            {
+                ans.a[i] = aa.a[i];                
+            }
+
+            for (int i = 0; i < 24; ++i)
+            {
+                if (ans.a[i] >= bb.a[i])
+                {
+                    ans.a[i] = ans.a[i] - bb.a[i];
+                }
+                else
+                {
+                    ans.a[i+1] -= 1;
+                    ans.a[i] = 10 + ans.a[i] - bb.a[i];
+                }
+            }
+        }
+        else
+        {
+            ans.flag = bb.flag;
+            for (int i = 0; i < 25; ++i)
+            {
+                ans.a[i] = bb.a[i];
+            }
+
+            for (int i = 0; i < 24; ++i)
+            {
+                if (ans.a[i] >= aa.a[i])
+                {
+                    ans.a[i] = ans.a[i] - aa.a[i];
+                }
+                else
+                {
+                    ans.a[i+1] -= 1;
+                    ans.a[i] = 10 + ans.a[i] - aa.a[i];
+                }
+            }
+        }
+    }
+    return ans;
+}
+#endif
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        string s1, s2, s3;
+        cin >> s1 >> s2 >> s3;
+        BigInt a(s1);
+        BigInt b(s2);
+        BigInt c(s3);
+        c.flag = !c.flag;
+        BigInt ans = a + b + c;
+        if (ans.greaterZero())
+        {
+            cout << "Case #" << i + 1 << ": true" << endl;
+        }
+        else
+        {
+            cout << "Case #" << i + 1 << ": false" << endl;
+        }
+    }
+    return 0;
+}
+
+
