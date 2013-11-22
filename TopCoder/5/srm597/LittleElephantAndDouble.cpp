70e545daf0a2e70b0fd6462c73f193c4eba0a220
python27 <silfer.lee@gmail.com>
2013-11-22 23:08:56 +0800

    the original solution not passed the system test, debug the condition for it , now OK

--- TopCoder/5/srm597/LittleElephantAndDouble.cpp
+++ TopCoder/5/srm597/LittleElephantAndDouble.cpp
@@ -2,22 +2,44 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 
+class LittleElephantAndDouble 
+{
+public:
 string getAnswer(vector<int> A)
 {
-    int max = max_element(A.begin(), A.end());
+    int max = *max_element(A.begin(), A.end());
     for (int i = 0; i < A.size(); ++i)
     {
-        if ( (max / A[i]) != 1 && (max / A[i]) % 2 != 0 )
+        // WARNING:
+        // either equal the max or divisible by max, the 
+        // answer can also divisible by 2, return YES
+        if ( max != A[i] && !(max % A[i] == 0 && (max / A[i]) % 2 == 0) )
         {
             return "NO"; 
         }
     }
     return "YES";
 }
+};
 
-int main()
-{
-    return 0;
-}
+
+// Summary:
+// the original condition is like
+// *******************************************
+// * max / A[i] == 1 || (max / A[i]) % 2 == 0
+// *******************************************
+// which is WRONG, because if 5 / 3 also equals 1, this condition 
+// neglect that the division between integer is not accurate
+// the right condition is that:
+// *************************************************************
+// * the element is either equals the max, or the max is divisible 
+// * the element && (max / element) % 2 == 0
+// *  !( max == A[i] || (max % A[i] == 0 && (max / A[i]) % 2 == 0) )
+// * which is equaivent to
+// * max != A[i] && !(max % A[i] == 0 && (max / A[i]) % 2 == 0)
+// 
+// python27
+// 2013/11/22
