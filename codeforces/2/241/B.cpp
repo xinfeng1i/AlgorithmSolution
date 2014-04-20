c51d651cc1fbb1952103a5872bfd42c090ec9149
python27 <silfer.lee@gmail.com>
2014-04-20 13:13:07 +0800

    DP problem, not figure it out, refer others' elegant code

--- codeforces/2/241/B.cpp
+++ codeforces/2/241/B.cpp
@@ -3,18 +3,42 @@
 #include <vector>
 using namespace std;
 
+int dp[50000+5][5+5];
+int t;
+
+/**
+ * DP problem:
+ * 
+ * dp[i][j] = the time when the ith picture done by the jth painter 
+ * then 
+ * dp[i][j] = max(dp[i-1][j] + t, dp[i][j-1] + t)
+ * initial condition
+ * dp[0][j] = {0};
+ * dp[i][0] = {0};
+ *
+ */
+
 int main()
 {
 	long m, n;
 	cin >> m >> n;
-	vector<vector<int> > v(m, vector<int>(n, 0) );
-	for (int i = 0; i < m; ++i)
+
+	
+	for (int i = 1; i <= m; ++i)
 	{
-		for (int j = 0; j < n; ++j)
+		for (int j = 1; j <= n; ++j)
 		{
-			scanf("%d", &v[i][j]);
+			cin >> t;
+			dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + t;
 		}
 	}
 
+	for (int i = 1; i <= m; ++i)
+	{
+		cout << " " << dp[i][n];
+	}
+	cout << endl;
+
+	return 0;
 
 }
