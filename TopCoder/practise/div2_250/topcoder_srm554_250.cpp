65d6c65512df3c6f40eb6f45fd41db2ecdefc2b1
python27 <silfer.lee@gmail.com>
2014-05-01 22:01:19 +0800

    tricky, TODO

---
+++ TopCoder/practise/div2_250/topcoder_srm554_250.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class TheBrickTowerEasyDivTwo
+{
+    public:
+        int find(int redCount, int redHeight, int blueCount, int blueHeight)
+        {
+            int a = min(redCount, blueCount);
+            if (redHeight == blueHeight)
+            {
+                
+            }
+        }
+};
