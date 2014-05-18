d6d133d5d0c03895fff8927e593aa4be245e30c8
python27 <silfer.lee@gmail.com>
2014-05-19 00:34:44 +0800

    index tree, parent pointer,good

--- Pat/dataStructure/4-05.cpp
+++ Pat/dataStructure/4-05.cpp
@@ -8,25 +8,24 @@
 #include <functional>
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
 using namespace std;
-
-struct MyName
-{
-	int level;
-	string name;
-};
 
 struct Node
 {
-	MyName levelName;
-	vector<Node*> pChildren;
-	Node(MyName aname)
-	{
-		levelName.level = aname.level;
-		levelName.name = aname.name;
-	}
+	string name_;
+	int level_;
+	Node* parent_;
+	vector<Node*> children_;
+	Node(){};
 };
 
+void printNode(Node* p)
+{
+	cout << "---Node Name:" << p->name_ << endl;
+	cout << (p->parent_ == NULL ? "NULL" : p->parent_->name_) << endl;
+	cout << "---end" << endl;
+}
 
 
 
@@ -36,8 +35,10 @@ int main()
 	int n, m;
 	scanf("%d %d\n", &n, &m);
 	
-	string line, name;
-	MyName myname;
+	map<string, Node*> table;
+	string line, curName;
+	int curLevel;
+	Node* curNode = NULL;
 	for (int i = 0; i < n; ++i)
 	{
 		getline(cin, line);
@@ -47,20 +48,139 @@ int main()
 			if (line[k] == ' ') nspace++;
 			else break;
 		}
-		name = line.substr(nspace);
-		myname.level = nspace / 2;
-		myname.name = name;
-		//cout << "My Name:" << myname.level << " " << myname.name << endl;
+		curName = line.substr(nspace);
+		curLevel = nspace / 2;
+		
+		if (curNode == NULL)
+		{
+			curNode = new Node();
+			curNode->name_ = curName;
+			curNode->level_ = curLevel;
+			curNode->parent_ = NULL;
+			table[curName] = curNode;
+		}
+		else
+		{
+			Node* newNode =	new Node();
+			newNode->name_ = curName;
+			newNode->level_ = curLevel;
+			
+			// find the parent of newNode, pointed by curNode;
+			if (newNode->level_ <= curNode->level_)
+			{
+				int btrack = curNode->level_ - newNode->level_ + 1;
+				for (int k = 0; k < btrack; ++k)
+				{
+					curNode = curNode->parent_;
+				}
+
+			}
+
+			newNode->parent_ = curNode;
+			curNode->children_.push_back(newNode);
+			table[newNode->name_] = newNode;
+
+			//update curNode repoint the newNode
+			curNode = newNode;
+		}
+
 	}
 
 	string nameX, isword, aword, nameY, ofword, relation;
 	for (int i = 0; i < m; ++i)
-	{
-		getline(cin, line);
+	{	
+		getline(cin, line); 
 		istringstream iss(line);
 		iss >> nameX >> isword >> aword >> relation >> ofword >> nameY;
-		//cout << nameX << " " << nameY << " " << relation << endl;
+		
+		if (relation == "child")
+		{
+			if (table[nameX]->parent_ == table[nameY])
+			{
+				cout << "True" << endl;
+			}
+			else
+			{
+				cout << "False" << endl;
+			}
+		}
+		else if (relation == "parent")
+		{
+			if (table[nameY]->parent_ == table[nameX])
+			{
+				cout << "True" << endl;
+			}
+			else
+			{
+				cout << "False" << endl;
+			}
+		}
+		else if (relation == "sibling")
+		{
+			if (table[nameX]->parent_ == table[nameY]->parent_)
+			{
+				cout << "True" << endl;
+			}
+			else
+			{
+				cout << "False" << endl;
+			}
+		}
+		else if (relation == "descendant")
+		{
+			Node* p = table[nameX];
+			assert(p != table[nameY]);
+			int cnt = 0;
+			while (p != NULL && p != table[nameY])
+			{
+				p = p->parent_;
+				cnt++;
+			}
+			if (p == table[nameY] && cnt >= 1)
+			{
+				cout << "True" << endl;
+			}
+			else
+			{
+				cout << "False" << endl;
+			}
+		}
+		else if (relation == "ancestor")
+		{
+			Node* py = table[nameY];
+			int cnt = 0;
+			while (py != NULL && py != table[nameX])
+			{
+				py = py->parent_;
+				cnt++;
+			}
+			if (py == table[nameX] && cnt >= 1)
+			{
+				cout << "True" << endl;
+			}
+			else
+			{
+				cout << "False" << endl;
+			}
+		}
+		else
+		{
+			cerr << "Error" << endl;
+			return -1;
+		}
 	}
+
+	for (auto it = table.begin(); it != table.end(); ++it)
+	{
+		delete it->second;
+	}
+
+	/*
+	for (auto it = table.begin(); it != table.end(); ++it)
+	{
+		printNode(it->second);
+	}
+	*/
 
 	return 0;
 }
