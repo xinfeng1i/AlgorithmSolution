3d56eb1aa1d4d30e56b6840abcc731a89ffb65b4
python27 <silfer.lee@gmail.com>
2014-05-20 00:12:01 +0800

    last sample TLE, todo

--- Pat/dataStructure/5-07.cpp
+++ Pat/dataStructure/5-07.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
 #include <algorithm>
 #include <functional>
 #include <cstdio>
@@ -23,30 +24,56 @@ int main()
 	freopen("input.txt", "r", stdin);
 	map<string, int> table;
 	int n = 0;
-	cin >> n;
-	cin.ignore();
+	scanf("%d\n", &n);
 
 	string line;
 	for (int i = 0; i < n; ++i)
 	{
 		getline(cin, line);	
-		int pos1 = 0;
-		while (line[pos1] != '#') pos1++;
-		int pos2 = pos1 + 1;
-		while (line[pos2] != '#') pos2++;
-		string topic = line.substr(pos1+1, pos2 - pos1 - 1);
-		string finalTopic;
-		finalTopic.reserve(topic.size());
-		for (size_t i = 0; i < topic.size(); ++i)
+		
+		int curPos = 0;
+		int lsz = line.size();
+
+		set<string> curLineTopics;
+		while (curPos < lsz)
 		{
-			if (isalpha(topic[i]) || isdigit(topic[i]) || isspace(topic[i]))
+			int pos1 = curPos;
+			while (pos1 < lsz && line[pos1] != '#') pos1++;
+			int pos2 = pos1 + 1;
+			while (pos2 < lsz && line[pos2] != '#') pos2++;
+			if (pos1 >= lsz || pos2 >= lsz)
 			{
-				finalTopic.push_back(tolower(topic[i]));
+				break;
 			}
+
+			string topic = line.substr(pos1+1, pos2 - pos1 - 1);
+			string finalTopic;
+			finalTopic.reserve(topic.size());
+			for (size_t i = 0; i < topic.size(); ++i)
+			{
+				if (isalpha(topic[i]) || isdigit(topic[i]) || isspace(topic[i]))
+				{
+					finalTopic.push_back(tolower(topic[i]));
+				}
+				if (topic[i] == '@')
+				{
+					finalTopic.push_back(' ');
+				}
+			}
+			//cout << "final topic:" << finalTopic << endl;
+			if (finalTopic.size() > 40) finalTopic = finalTopic.substr(0, 40);
+			
+			curLineTopics.insert(finalTopic);
+			
+			//table[finalTopic]++;
+
+			curPos = pos2 + 1;
 		}
-		//cout << "final topic:" << finalTopic << endl;
-		if (finalTopic.size() > 40) finalTopic = finalTopic.substr(0, 40);
-		table[finalTopic]++;
+
+		for (auto it = curLineTopics.begin(); it != curLineTopics.end(); ++it)
+		{
+			table[*it]++;
+		}
 	}
 
 	vector<pair<string, int> > v;
