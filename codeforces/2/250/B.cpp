5004222328251d31624aec75b8c2643c6e859573
python27 <silfer.lee@gmail.com>
2014-06-08 21:21:05 +0800

    lowbit refer other's elegant code, greedy algorithm need more practice

--- codeforces/2/250/B.cpp
+++ codeforces/2/250/B.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
+
+int lowbit(int x)
+{
+	int ans = 1;
+	while ((x & 1) == 0)
+	{
+		x = (x >> 1);
+		ans *= 2;
+	}
+	return ans;
+}
+
+/* the lowbit function can also be compute by the following
+ *
+ * refer other's elegant code
+ */
+int lowbit2(int x)
+{
+    return x & (-x);
+}
 
 int main()
 {
-	int x = 0;
-	cin >> x;
-	
-	int ans = x - ((x >> 2) << 2);
-	cout << ans << endl;
+    int sum, limit;
+    cin >> sum >> limit;
+
+    vector<int> v;
+    v.reserve(limit);
+    for (int x = limit; x >= 1 && sum > 0; --x)
+    {
+        int low = lowbit(x);
+        if (low <= sum)
+        {
+            sum -= low;
+            v.push_back(x);
+        }
+    }
+    
+    if (sum != 0)
+    {
+        cout << -1 << endl;
+        return 0;
+    }
+    else
+    {
+        cout << v.size() << endl;
+        for (size_t i = 0; i < v.size(); ++i)
+        {
+            if (i != 0) cout << " " << v[i];
+            else cout << v[i];
+        }
+        cout << endl;
+    }
 	return 0;
 }
