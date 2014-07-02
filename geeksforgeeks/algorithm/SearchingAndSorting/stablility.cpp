a238490bd07a8ce075c913be795bd54b8802aea5
python27 <silfer.lee@gmail.com>
2014-07-02 21:39:47 +0800

    conclusions on sorting algorithms

---
+++ geeksforgeeks/algorithm/SearchingAndSorting/stablility.cpp
@@ -0,0 +1,18 @@
+
+/* Stablity
+ * stable:   Insertion Sort, Buddle Sort, Merge Sort
+ * unstable: Quick Sort, Heap Sort
+ */
+
+/* Quick Sort worst occurs
+ * 1. Array is already sorted in same order (reverse order)
+ * 2. All elements are same
+ */
+
+/* Lower Bound on comparsion based sorting algorithms:
+ * O(nlogn)
+ */
+
+/* Which sorting algorithm makes minimum number of memory writes
+ * quick sort
+ */
