f721dda1b752e708117bc00179d183f68db8aff7
python27 <silfer.lee@gmail.com>
2014-09-03 15:53:09 +0800

    change to scanf/printf, memory exceed

--- Pat/advanced/1039.cpp
+++ Pat/advanced/1039.cpp
@@ -5,37 +5,47 @@
 #include <algorithm>
 #include <string>
 #include <cstdio>
+#include <cmath>
 using namespace std;
 
+// 最后一个sample超出内存，主要原因是因为姓名/课程表占用内存太多
+// 引起的，需要压缩学生姓名与课程名称，AC代码见1039_2.cpp
 int main()
 {
-    //freopen("1039data.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
 
+    char stuName[5];
     long queryNumber = 0;
     int courseNumber = 0;
-    cin >> queryNumber >> courseNumber;
+    //cin >> queryNumber >> courseNumber;
+    scanf("%ld %d", &queryNumber, &courseNumber); 
 
     map<string, set<int> > table;
     int courseIndex = 0;
     long studentNumber = 0;
-    string name;
+    //string name;
     for (int i = 0; i < courseNumber; ++i)
     {
-        cin >> courseIndex >> studentNumber;
+        //cin >> courseIndex >> studentNumber;
+        scanf("%d %ld", &courseIndex, &studentNumber);
         for (int k = 0; k < studentNumber; ++k)
         {
-            cin >> name;
+            //cin >> name;
+            scanf("%s", stuName);
+            string name(stuName);
             table[name].insert(courseIndex);
         }
     }
 
     vector<string> querylist(queryNumber);
-    string queryName;
     for (long i = 0; i < queryNumber; ++i)
     {
-        cin >> queryName;
+        //cin >> queryName;
+        scanf("%s", stuName);
+        string queryName(stuName);
         int registerNumber = table[queryName].size();
-        cout << queryName << " " << registerNumber;
+        //cout << queryName << " " << registerNumber;
+        printf("%s %d", stuName, registerNumber);
         if (registerNumber == 0)
         {
             //cout << endl;
