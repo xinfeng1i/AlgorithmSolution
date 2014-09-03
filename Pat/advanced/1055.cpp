02b4d299f44fb1b0ba02e414d069267d443d5082
python27 <silfer.lee@gmail.com>
2014-09-03 10:38:25 +0800

    add the filter rules to make search faster

--- Pat/advanced/1055.cpp
+++ Pat/advanced/1055.cpp
@@ -48,6 +48,22 @@ int main()
 
     sort(richList.begin(), richList.end(), compareRich);
 
+    // 对于同一年龄出现次数超过100后的人根本不能登上排行榜
+    // 所以需要过滤掉这些人，加快搜索速度
+    vector<int> filter(totalNum, 0);
+    int filterNum = 0;
+    vector<int> ageCnt(201, 0);
+    for (size_t i = 0; i < richList.size(); ++i)
+    {
+        int curAge = richList[i].age_;
+        ageCnt[curAge] += 1;
+        if (ageCnt[curAge] <= 100)
+        {
+            filter[filterNum] = i;
+            filterNum++;
+        }
+    }
+
     int outputNum = 0;
     int minAge = 201;
     int maxAge = -1;
@@ -63,6 +79,7 @@ int main()
         }
 
         int count = 0;
+        /*
         for (auto it = richList.begin(); count < outputNum && 
                                          it != richList.end(); ++it)
         {
@@ -70,6 +87,16 @@ int main()
             {
                 printf("%s %d %ld\n", it->name_, it->age_, it->worth_);
                 count++; 
+            }
+        }
+        */
+        for (int k = 0; count < outputNum && k < filterNum; k++)
+        {
+            int i = filter[k];
+            if (richList[i].age_ >= minAge && richList[i].age_ <= maxAge)
+            {
+                printf("%s %d %ld\n", richList[i].name_, richList[i].age_, richList[i].worth_);
+                count++;
             }
         }
 
