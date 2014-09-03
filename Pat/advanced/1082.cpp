7b7224ff1c49328f7c226bee27b28aced0aef330
python27 <silfer.lee@gmail.com>
2014-09-04 00:41:46 +0800

    tood

---
+++ Pat/advanced/1082.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string to_chinese(char ch)
+{
+	switch(ch)
+	{
+		case '0':
+			return "ling";
+			break;
+		case '1':
+			return "yi";
+			break;
+		case '2':
+			return "er";
+			break;
+		case '3':
+			return "san";
+			break;
+		case '4':
+			return "si";
+			break;
+		case '5':
+			return "wu";
+			break;
+		case '6':
+			return "liu";
+			break;
+		case '7':
+			return "qi";
+			break;
+		case '8':
+			return "ba";
+			break;
+		case '9':
+			return "jiu";
+			break;
+	}
+}
+
+string read4num(string s)
+{
+	int pos = 0;
+	while (s[pos] == '0') pos++;
+	string ans;
+	switch(pos)
+	{
+		case 0:
+			ans += to_chinese(s[0]) + " Qian";
+			if (s.substr(1)  == "000") return ans;
+
+			if (s[1] == '0') ans += " " + to_chinese('0');
+			else ans += " " + to_chinese(s[1]) + " Bai";
+			if (s.substr(2) == "00") return ans;
+
+			if (s[1] == '0' && s[2] == '0') ans += "";
+			else if (s[1] != '0' && s[2] == '0') ans += " " + to_chinese('0');
+			else ans += " " + to_chinese(s[2]) + " Shi";
+			if (s.substr(3) == "0") return ans;
+
+			ans += " " + to_chinese(s[3]);
+			return ans;
+		case 1:
+			ans += to_chinese(s[1]) + " Bai";
+			if (s.substr(2) == "00") return ans;
+
+			if (s[2] == '0') 
+			{
+				ans += " ling " + to_chinese(s[3]);
+				return ans;
+			}
+			else 
+			{
+				ans += " " + to_chinese(s[2]) + " Shi";
+				if (s[3] == '0') return ans;
+				else
+				{
+					ans += " " + to_chinese(s[3]);
+					return ans;
+				}
+			}
+		case 2:
+			if (s[3] == '0')
+			{
+				ans += to_chinese(s[2]) + " Shi";
+			}
+			else
+			{
+				ans += to_chinese(s[2]) + " Shi " + to_chinese(s[3]);
+			}
+			return ans;
+		case 3:
+			ans += to_chinese(s[3]);
+			return ans;
+		default:
+			return "Not done";
+	}
+}
+
+
+int main()
+{
+	string s;
+	cin >> s;
+	bool flag = true;
+	if (s[0] == '-')
+	{
+		flag = false;
+		s = s.substr(1);
+	}
+	
+	if (!flag)
+	{
+		cout << "Fu ";
+	}
+
+
+	if (s.size() <= 4)
+	{
+		if (s == "0000" || s == "000" || s == "00" || s == "0")
+		{
+			cout << "ling" << endl;
+			return 0;
+		}
+		else
+		{
+			while (s.size() < 4) s = "0" + s;
+			cout << read4num(s) << endl;
+			return 0;
+		}
+	}
+	else if (s.size() <= 8)
+	{
+		string qian4wei = s.substr(0, s.size() - 4);
+		string hou4wei  = s.substr(s.size() - 4);
+
+		if (qian4wei == "0000" || qian4wei == "000" || qian4wei == "00" || qian4wei == "0")
+		{
+			if (hou4wei == "0000")
+			{
+				cout << "ling" << endl;
+			}
+			else
+			{
+				cout << read4num(hou4wei) << endl;
+			}
+			return 0;
+		}
+		else
+		{
+			string ans;
+			while (qian4wei.size() < 4) qian4wei = "0" + qian4wei;
+			ans += read4num(qian4wei) + " Wan";
+			if (hou4wei == "0000")
+			{
+				return ans;
+			}
+			else
+			{
+				int pos = 0;
+				while (hou4wei[pos] == '0') pos++;
+				if (pos >= 1) ans += " ling";
+				ans += " " + read4num(hou4wei);
+				return ans;
+			}
+		}
+	}
+	else if (s.size() <= 9)
+	{
+		
+	}
+
+	
+}
