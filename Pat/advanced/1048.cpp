2b8bc4bb95f96c8e1eeea683f56b33b8ba945dc6
python27 <silfer.lee@gmail.com>
2014-09-04 21:55:14 +0800

    use binary search to lower complexity

--- Pat/advanced/1048.cpp
+++ Pat/advanced/1048.cpp
@@ -8,6 +8,32 @@
 #include <cstdlib>
 using namespace std;
 
+int binarySearch(const vector<int>& v, int key)
+{
+    int n = v.size();
+    int left = 0;
+    int right = n - 1;
+    int mid = (left + right) / 2;
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+        if (key == v[mid])
+        {
+            return mid;
+        }
+        else if (key < v[mid])
+        {
+            right = mid - 1;
+        }
+        else
+        {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
 int main()
 {
     unsigned int n, m;
@@ -22,19 +48,26 @@ int main()
 
     for (size_t i = 0; i < n; ++i)
     {
-        for (size_t j = i + 1; j < n; ++j)
+        int y = m - v[i];
+        int j = binarySearch(v, y);
+        if (j == -1) 
         {
-            if (v[i] + v[j] == m)
+            continue;
+        }
+        else
+        {
+            if (i != j && v[i] <= v[j])
             {
-                printf("%d %d\n", v[i], v[j]);
+                cout << v[i] << " " << v[j] << endl;
                 return 0;
-            }
-            else if (v[i] + v[j] > m)
-            {
-                break;
             }
         }
     }
     printf("No Solution\n");
     return 0;
 }
+
+/*
+ * 采用二分查找可以将搜索时间从原来的O(N^2)
+ * 降低到(NlogN)
+ */
