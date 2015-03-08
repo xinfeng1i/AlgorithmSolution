d5352e9de965631eaefc2607685bacd8003b4d98
python27 <silfer.lee@gmail.com>
2015-03-08 22:12:22 +0800

    k-way merge using minHeap (STL prioprity_queue, greater<int>), good, need recheck

--- leetcode/MergeKSortedLists.cpp
+++ leetcode/MergeKSortedLists.cpp
@@ -1,85 +1,101 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
 using namespace std;
 
-struct ListNode
+struct ListNode 
 {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL){}
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
 };
 
-ListNode* mergeKLists(vector<ListNode*> &lists)
+ListNode *mergeKLists(vector<ListNode *> &lists)
 {
-    int n = lists.size();
-    if (n == 0) return NULL;
-    if (n == 1) return lists[0];
+	ListNode* newList = NULL;
+	ListNode* preNode = NULL;
 
-    vector<ListNode*> p(n);
-    for (int i = 0; i < n; ++i)
-    {
-        p[i] = lists[i];
-    }
+	priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > minHeap;
 
-    ListNode* head = NULL;
-    ListNode* cur = NULL;
+	int n = lists.size();
 
-    while (true)
-    {
-        // point to the first Not NULL node, pivot
-        ListNode* minP = new ListNode(1000000000);
-        int minI = -1;
-        for (int i = 0; i < n; ++i)
-        {
-            if (p[i] != NULL && p[i]->val < minP->val)
-            {
-                minP = p[i];
-                minI = i;
-            }
-        }
 
-        // all list are empty, terminate loop
-        if (minI == -1) break;
-        
-        // minP ponit to the min node
+	// push the first n elements into a Min-Heap
+	int k = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (lists[i] != NULL) k++;
+	}
+	if (k == 0) return NULL;
 
-        if (cur == NULL)
-        {
-            head = minP;
-            cur = minP;
-        }
-        else
-        {
-            cur->next = minP;
-            cur = minP;         // update current point
-        }
-        p[minI] = p[minI]->next;
-    }
+	// Construct a min-Heap with the head-K elements
+	for (int i = 0; i < n; ++i)
+	{
+		if (lists[i] != NULL)
+		{
+			minHeap.push(make_pair(lists[i]->val, i));
+		}
+	}
 
-    return head;
+	// remove the min element, and insert to the answer list
+	// until the heap is empty
+	while (minHeap.size() > 0)
+	{
+		// pop the minimum element
+		auto tmp = minHeap.top();
+		minHeap.pop();
+
+		// insert it into the answer list
+		ListNode* pNewNode = new ListNode(tmp.first);
+		if (preNode != NULL)
+		{
+			preNode->next = pNewNode;
+			preNode = pNewNode;
+		}
+		else
+		{
+			newList = pNewNode;
+			preNode = pNewNode;
+		}
+
+		// the pointer move to next
+		lists[tmp.second] = lists[tmp.second]->next;
+		// if not NULL, push the next element into heap so that
+		// the heap size is still k
+		// else not push element, the heap size is k - 1
+		if (lists[tmp.second] != NULL)
+		{
+			minHeap.push(make_pair(lists[tmp.second]->val, tmp.second));
+			//cout << "push:" << lists[tmp.second]->val << endl;
+		}
+	}
+
+
+	return newList;
 }
 
 int main()
 {
-    vector<ListNode*> lists(3);
-    lists[0] = new ListNode(1);
-    lists[0]->next = new ListNode(3);
-    lists[0]->next->next = new ListNode(7);
+	ListNode* L1 = new ListNode(1);
+	L1->next = new ListNode(2);
+	L1->next->next = new ListNode(3);
 
-    lists[1] = new ListNode(2);
-    lists[1]->next = new ListNode(6);
-    lists[1]->next->next = new ListNode(9);
+	ListNode* L2 = new ListNode(4);
+	L2->next = new ListNode(5);
+	L2->next->next = new ListNode(6);
 
-    lists[2] = new ListNode(5);
-    lists[2]->next = new ListNode(8);
-    lists[2]->next->next = new ListNode(10);
-    
-    ListNode* head = mergeKLists(lists);
-    for (ListNode* p = head; p != NULL; p = p->next)
-    {
-        cout << " " << p->val;
-    }
-    cout << endl;
-    return 0;
+	vector<ListNode*> v;
+	v.push_back(L1);
+	v.push_back(L2);
+
+	ListNode* ans = mergeKLists(v);
 }
