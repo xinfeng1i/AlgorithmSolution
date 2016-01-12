dff5ff2ac86f29cc064efb23c122c9beae447e30
python27 <silfer.lee@gmail.com>
2016-01-12 22:21:39 +0800

    classical backtracking problem, easy good

---
+++ leetcode/52 N-Queens II.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool IsSafe(int row, int col, vector<string>& grid)
+{
+    int n = grid.size();
+    // check column
+    for (int i = 0; i < row; ++i) {
+        if (grid[i][col] == 'Q')
+            return false;
+    }
+    
+    // check left-up diag
+    for (int i = row-1, j = col-1; i>=0 && j>=0; i--,j--) {
+        if (grid[i][j] == 'Q')
+            return false;
+    }
+    
+    // check right-up diag
+    for (int i = row-1, j = col+1; i>=0 && j<n; i--, j++) {
+        if (grid[i][j] == 'Q') {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void NQueensRecursive(int row, vector<string>& grid, vector<vector<string> >& result)
+{
+    int n = grid.size();
+    if (row >= n) {
+        result.push_back(grid);
+        return;
+    }
+    
+    for (int j = 0; j < n; ++j) {
+        if(IsSafe(row, j, grid)) {
+            // try a choice
+            grid[row][j] = 'Q';
+            NQueensRecursive(row+1, grid, result);
+            // backtracking
+            grid[row][j] = '.';
+        }
+    }
+    return;
+}
+
+int totalNQueens(int n) {
+	vector<string> grid(n, string(n, '.'));
+	vector<vector<string> > result;
+	NQueensRecursive(0, grid, result);
+	return result.size();
+}
