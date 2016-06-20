5ca5a1c3626b7bacdaf66d94fcf536faa76b690d
python27 <silfer.lee@gmail.com>
2016-06-20 11:24:36 +0800

    Dynamic Problem Analysis and Solve it elegantly, very good, I am genius

--- leetcode/97 Interleaving String.cpp
+++ leetcode/97 Interleaving String.cpp
@@ -3,49 +3,44 @@
 #include <vector>
 using namespace std;
 
-
-inline string maxCommonPrefix(const string& a, const string& b) {
-    size_t n1 = a.size(); size_t n2 = b.size();
-    size_t i = 0;
-    while (i < n1 && i < n2 && a[i] == b[i]) {
-        i++;
-    }
-    return a.substr(0, i);
-}
-
 bool isInterleave(string s1, string s2, string s3) {
-    if (s1.empty()) return s2 == s3;
-    if (s2.empty()) return s1 == s3;
     if (s3.size() != s1.size() + s2.size()) return false;
     
-    string common_str1 = maxCommonPrefix(s3, s1);
-    //cout << common_str1 << endl;
-    for (size_t i = 1; i <= common_str1.size(); ++i) {
-        string next_s1 = s1.substr(i);
-        string next_s3 = s3.substr(i);
-        bool ok = isInterleave(next_s1, s2, next_s3);
-        if (ok) {
-            return true;
-        }
+    int m = (int) s1.size();
+    int n = (int) s2.size();
+    // dp[i][j] = the first i chars in s1, the first j chars in s2
+    // can or not can construct to the first (i+j) chars in s3
+    vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
+    // base case
+    for (int len = 0; len <= n; ++len) {
+        dp[0][len] = (s2.substr(0, len) == s3.substr(0, len));
+    }
+    for (int len = 0; len <= m; ++len) {
+        dp[len][0] = (s1.substr(0, len) == s3.substr(0, len));
     }
     
-    string common_str2 = maxCommonPrefix(s3, s2);
-    for (size_t i = 1; i <= common_str2.size(); ++i) {
-        string next_s2 = s2.substr(i);
-        string next_s3 = s3.substr(i);
-        bool ok = isInterleave(s1, next_s2, next_s3);
-        if (ok) {
-            return true;
+    // bottom to up
+    // the last char in s3[i+j-1] either comes from s1[i-1] or s2[j-1]
+    // one of them lead to true, then the result is true
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            dp[i][j] = false;
+            if (s1[i-1] == s3[i+j-1] && dp[i-1][j] == true) {
+                dp[i][j] = true;
+            }
+            if (s2[j-1] == s3[i+j-1] && dp[i][j-1] == true) {
+                dp[i][j] = true;
+            }
         }
     }
-    return false;
+    return dp[m][n];
 }
 
-int main()
-{
-	string s1 = "a";
-	string s2 = "b";
-	string s3 = "ab";
+
+int main() {
+	string s1 = "aabc";
+	string s2 = "abad";
+	string s3 = "aabadabc";
 	cout << isInterleave(s1, s2, s3) << endl;
 	return 0;
 }
