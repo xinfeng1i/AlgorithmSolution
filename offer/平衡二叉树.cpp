fdbbad6edcf520dc703028bac22a493c7773508e
python27 <silfer.lee@gmail.com>
2016-10-06 14:46:31 +0800

    add comment

--- offer/平衡二叉树.cpp
+++ offer/平衡二叉树.cpp
@@ -5,6 +5,11 @@
 #include <algorithm>
 using namespace std;
 
+/*
+ * 该函数判断当前root为根的树是否为BST，同时返回
+ * 该树的深度depth
+ * 1. 注意递归结束时更新需要输出的depth值
+ */
 bool BSTWithDepth(TreeNode* root, int& depth) {
 	if (root == NULL) {
 		depth = 0;
@@ -13,6 +18,7 @@ bool BSTWithDepth(TreeNode* root, int& depth) {
 	int depth_left = 0, depth_right = 0;
 	bool leftBST = BSTWithDepth(root->left, depth_left);
 	bool rightBST = BSTWithDepth(root->right, depth_right);
+	// 返回结果之前需要输出depth的值
 	depth = max(depth_left, depth_right) + 1;
 	
 	return leftBST && rightBST && abs(depth_left - depth_right) <= 1;
