5519a07ab106e4b7e689ee02212a1de626c03f5f
xinfengli <xinfengli@tencent.com>
2022-05-05 23:21:21 +0800

    ADD: binary tree LCA problem, remember it good

---
+++ leetcode/Leetcode236_Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cassert>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <utility>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+// 二叉树最低公共祖先：经典问题
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (root == NULL) return NULL;
+    if (root == p || root == q) return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left == NULL && right == NULL) return NULL;
+    if (left == NULL && right != NULL) return right;
+    if (right == NULL && left != NULL) return left;
+
+    return root;
+}
